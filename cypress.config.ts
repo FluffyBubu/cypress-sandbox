6bdb4ca3669235fe888b6450a0407595fbfe788f
Marcus Harvey <marcus0228@gmail.com>
2023-06-10 23:12:27 +0200

    add Login test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
+    "baseUrl": "http://localhost:3000",
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
+    },
+    env: {
+      email: 'foo@bar.com',
+      password: '123456'
     },
   },
 })
