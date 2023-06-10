eaada66e50a60f4e9d26fb4df81926c3da4f5b6e
Marcus Harvey <marcus0228@gmail.com>
2023-06-10 19:56:20 +0200

    add Filip Hric's trello app

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'public/css/trello.css': 'assets/scss/trello.scss'
+        }
+      }
+    },
+    browserify: {
+      dist: {
+        files: {
+          'public/js/trello.js': 'assets/js/trello/main.js',
+        },
+      },
+      options: {
+        transform: [['babelify']],
+        watch: true
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass'],
+      },
+      scripts: {
+        files: './assets/**/*.js',
+        tasks: ['browserify'],
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('s', ['sass']);
+  grunt.registerTask('b', ['browserify']);
+  grunt.registerTask('dev', ['sass', 'browserify']);
+
+};
