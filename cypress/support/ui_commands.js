368b22f2f3cb9bccd7aa0c3dacd8b527998cd2ca
Marcus Harvey <marcus0228@gmail.com>
2023-06-11 08:00:35 +0200

    add getByData command

--- cypress/support/ui_commands.js
+++ cypress/support/ui_commands.js
@@ -1,12 +1,16 @@
+Cypress.Commands.add('getByData', (selector) => {
+    return cy.get(`[data-cy=${selector}]`)
+})
+
 Cypress.Commands.add('login_ui', (email, password) => {
     Cypress.log({ displayName: 'Login using UI' })
     
     // Fill email
-    cy.get('[data-cy="login-email"]').type(email)
+    cy.getByData('login-email').type(email)
 
     // Fill password
-    cy.get('[data-cy="login-password"]').type(password)
+    cy.getByData('login-password').type(password)
 
     // Click login button
-    cy.get('[data-cy="login"]').click()
+    cy.getByData('login').click()
 })
