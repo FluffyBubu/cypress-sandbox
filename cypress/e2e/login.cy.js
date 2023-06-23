21661985a2c3a12b5710908a4edb8284124c736a
Marcus Harvey <marcus0228@gmail.com>
2023-06-23 13:15:59 +0200

    add API Login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,22 @@
+/* 
+    MODEL/DESIGN
+    Cypress design with API and UI actions.
+
+    DESCRIPTION
+    This is a simple Login feature with a positive and negative test case using both API and UI actions.
+    The API is used to clean up the database and to create a user.
+    It is also used to perform the login action.
+    The UI is used to check that the user is logged in or not.
+    
+    STRENGTH: 
+    This design can be speed up your tests drastically by using API calls to perform actions 
+    that would otherwise take a long time to perform via UI.
+
+    CHALLENGE: 
+    You need to be sure that you understand how to use your API and have placed the correct assertions
+    in order to avoid false positives (flaky tests).
+*/
+
 describe('Login', () => {
     const email = Cypress.env('email')
     const password = Cypress.env('password')
@@ -8,43 +27,55 @@ describe('Login', () => {
 
         // Test setup
         cy.api_createUser(email, password).its('status').should('equal', 201)
-
-        // Listen for API calls
-        cy.intercept('POST', '**/login').as('login_POST')
-
-        // Visit homepage
-        cy.visit('/')
-
-        // Check login button is displayed
-        cy.getByData('login-menu').as('loginButton').should('be.visible')
-
-        // Check login button text
-        cy.get('@loginButton').invoke('text').should('contain', 'Log in')
-
-        // Click the login button
-        cy.get('@loginButton').click()
-
-        // Check that the login form is displayed
-        cy.contains('.LoginModule_title', 'Log in to your account').should('be.visible')
-
     })
 
     it('should login', () => {
-        // Login with valid input
-        cy.login_ui(email, password)
+        /* Actions performed via API */
 
-        // Check API response
-        cy.wait('@login_POST').its('response.statusCode').should('equal', 200)
+        // Login with valid input via API
+        cy.api_login(email, password)
+            .then(({ body, status }) => {
+                const { accessToken } = body
 
-        // Check that the login message is visible
-        cy.contains('#loginMessage', 'User is logged in').should('be.visible')
+                expect(status).to.equal(200)
+
+                // Check that the response contains an access token
+                expect(body).to.have.property('accessToken')
+
+                // Set the access token as a cookie
+                cy.setCookie('trello_token', accessToken)
+
+                // Check that the cookie was set
+                cy.getCookie('trello_token').should('have.property', 'value', accessToken)
+            })
+
+        /* Actions performed via UI */
+
+        // Navigate to home page (with cookie)
+        cy.visit('/')
+
+        // Check that the the user is logged in
+        cy.getByData('logged-user').invoke('text').should('contain', email)
     })
 
     it('should fail to login', () => {
-        // Login with invalid input
-        cy.login_ui(email + 'invalid', password + 'invalid')
+        /* Actions performed via API */
 
-        // Check API response
-        cy.wait('@login_POST').its('response.statusCode').should('equal', 400)
+        // Login with invalid input via API
+        cy.api_login(email + 'invalid', password + 'invalid')
+            .then(({ body, status }) => {
+                expect(status).to.equal(400)
+
+                // Check that the response contains an error message
+                expect(body).to.have.string('Cannot find user')
+            })
+
+        /* Actions performed via UI */
+
+        // Navigate to home page (without cookie)
+        cy.visit('/')
+
+        // Check that the user is not logged in
+        cy.getByData('login-menu').invoke('text').should('contain', 'Log in')
     })
 })
--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -1,13 +1,35 @@
 Cypress.Commands.add('api_resetDb', () => {
-    Cypress.log({ displayName: 'Reset Database' })
+    Cypress.log({
+        displayName: 'Reset Database'
+    })
 
-    cy.request({ method: 'POST', url: '/api/reset' })
-        // .its('status').should('equal', 204)
+    cy.request({
+        method: 'POST',
+        url: '/api/reset'
+    })
 })
 
 Cypress.Commands.add('api_createUser', (email, password) => {
-    Cypress.log({ displayName: 'Create User' })
+    Cypress.log({
+        displayName: 'Create User'
+    })
 
-    cy.request({ method: 'POST', url: '/api/signup', body: { email, password } })
-            // .its('status').should('equal', 201)
+    cy.request({
+        method: 'POST',
+        url: '/api/signup',
+        body: { email, password }
+    })
+})
+
+Cypress.Commands.add('api_login', (email, password) => {
+    Cypress.log({
+        displayName: 'Login using API'
+    })
+
+    cy.request({
+        method: 'POST',
+        url: '/api/login',
+        body: { email, password },
+        failOnStatusCode: false
+    })
 })
