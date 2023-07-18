dbcd1186736f8634bda96c8274524f5ecb9e952a
Marcus Harvey <marcus0228@gmail.com>
2023-07-19 00:56:18 +0200

    Updated README

--- cypress/e2e/login-bdd-without-cucumber.cy.js
+++ cypress/e2e/login-bdd-without-cucumber.cy.js
@@ -1,21 +1,3 @@
-/* 
-    MODEL/DESIGN
-    Cypress design with BDD using Gherkin syntax (Given, When, Then), without Cucumber.
-
-    DESCRIPTION
-    This is a simple Login feature with a positive and negative test scenario using BDD.
-    
-    STRENGTH: 
-    This alternative BDD design still has the benefit of being simple to read and understand using the Gherkin syntax, but without the added complexity of Cucumber and while staying within the Cypress ecosystem.
-    BDD is a great way to write tests that are easy to read and understand, and to get non-technical people involved in the testing process.
-
-    CHALLENGE: 
-    Even though this alternive BDD design solves the complexity issue of Cucumber, scenario-based tests (like Cucumber) are purposefully designed to have a tight scope in order to test a business requirement. 
-    This means that the tests don't necessarily cover all the edge cases, and you might end up with a lot of scenarios to cover all the edge cases.
-*/
-
-// EVERYTHING BELOW THIS LINE IS A BDD EXAMPLE
-
 /* Feature: Login functionality */
 
 const email = Cypress.env('email')
